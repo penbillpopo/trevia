5ede835f400f5e3d6a6a95bb960bbdf577588452
ban <ban@bandeMacBook-Pro.local>
2025-10-20 13:55:13 +0800

    新增account

--- server/libs/@ay-gosu/sequelize-models/src/account.ts
+++ server/libs/@ay-gosu/sequelize-models/src/account.ts
@@ -2,14 +2,11 @@ import {
   AutoIncrement,
   Column,
   DataType,
-  Default,
-  HasMany,
   PrimaryKey,
   Table,
 } from 'sequelize-typescript';
 import { Unique } from './extra-decorator';
 import { TimeStamps } from './timestamps';
-import { TradeBot } from './trade-bot';
 
 @Table({
   tableName: 'accounts',
@@ -30,17 +27,4 @@ export class Account extends TimeStamps<Account> {
 
   @Column(DataType.STRING)
   public name: string;
-
-  @Column(DataType.STRING)
-  public apiKey: string;
-
-  @Column(DataType.STRING)
-  public apiSecret: string;
-
-  @Default(false)
-  @Column(DataType.BOOLEAN)
-  public apiKeyValid: boolean;
-
-  @HasMany(() => TradeBot)
-  public tradeBots: TradeBot[];
 }
--- server/libs/@ay-gosu/sequelize-models/src/index.ts
+++ server/libs/@ay-gosu/sequelize-models/src/index.ts
@@ -2,31 +2,8 @@ import SequelizeHierarchy from '@ay/sequelize-hierarchy';
 import { Sequelize } from 'sequelize';
 import { ModelCtor } from 'sequelize-typescript';
 import { Account } from './account';
-import { BacktestLog } from './backtest-log';
-import { MarketData } from './market-data';
-import { Task } from './task';
-import { TaskTradeBot } from './task-trade-bot';
-import { TradeBot } from './trade-bot';
-import { TradeLog } from './trade-log';
-import { TradeStrategy } from './trade-strategy';
 
 export * from './account';
-export * from './backtest-log';
-export * from './market-data';
-export * from './task';
-export * from './task-trade-bot';
-export * from './trade-bot';
-export * from './trade-log';
-export * from './trade-strategy';
 
-export const Models: ModelCtor[] = [
-  Account,
-  TradeBot,
-  TradeStrategy,
-  Task,
-  TaskTradeBot,
-  BacktestLog,
-  TradeLog,
-  MarketData,
-];
+export const Models: ModelCtor[] = [Account];
 SequelizeHierarchy(Sequelize);
